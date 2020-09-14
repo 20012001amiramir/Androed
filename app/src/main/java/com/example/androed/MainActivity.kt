47f0f02057a3a5f8ec98eacebb6bdfc40c2972f6
Amir <20012001amiramir@gmail.com>
2020-09-14 05:54:18 +0300

    HomeWork Class, Inteface, e.t.c.

--- app/src/main/java/com/example/androed/MainActivity.kt
+++ app/src/main/java/com/example/androed/MainActivity.kt
@@ -7,5 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Amir = Student("МЯСО","коворк итиса")
+        Amir.makeNoise()
+        Amir.eat()
+        Amir.noize()
+        Amir.sleep()
+        println(Amir.fod)
+        println(Amir.hom)
+        println(Amir.habitat)
+        println(Amir.food)
+
     }
 }
