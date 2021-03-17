3a07bfe2646934fa1e0084c07ca7a3bfadc0fd2c
Amir <20012001amiramir@gmail.com>
2021-03-17 18:14:06 +0300

    Архитектура Погода

--- app/src/main/java/com/itis/template/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.itis.template
-
-import android.R.layout
-import android.app.ListActivity
-import android.content.Intent
-import android.os.Bundle
-import android.view.View
-import android.widget.ArrayAdapter
